bf4b10bb1aa3a2a00b727d2045d46d611bb8a1d0
Jasmine Washington <47457724+jazzymaya@users.noreply.github.com>
2021-03-09 15:40:07 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_heroku
 from pathlib import Path
 import os
 
@@ -131,3 +132,5 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
