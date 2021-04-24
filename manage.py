f4612155645ec13656ff59a894c2dc210caf6530
Jasmine Washington <47457724+jazzymaya@users.noreply.github.com>
2021-04-24 18:27:44 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 ALLOWED_HOSTS = ['*']
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebdjango.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
